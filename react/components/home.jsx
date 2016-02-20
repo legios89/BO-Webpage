8452bbed0ce6a18b74420bbed5dd7714ad548b06
egyedgabor <egyedg90@gmail.com>
2016-02-20 21:02:38 +0100

    made footer

--- react/components/home.jsx
+++ react/components/home.jsx
@@ -83,20 +83,41 @@ var Home = React.createClass({
                style={{background: '#525E64'}}>
           </div>
         </div>
-        <div className="row">
-          <div className="col-xs-12 col-md-6" style={{background: '#8775A7'}}>
-            <p>e-mail:</p>
+        <div className="row" style={{background: '#E9EDEF'}}>
+          <div className="block-footer col-xs-12 col-md-5 about-description"
+               style={{color: '#2C3E50'}}>
+            <span>Birkas Orsolya,<br></br>
+            Toborzási és Kiválasztási Tanácsadó</span>
           </div>
-          <div className="col-xs-12 col-md-6" style={{background: '#8877A9'}}>
-            <a href="">
-              <i className="fa fa-facebook"></i>
-            </a>
-            <a href="">
-              <i className="fa fa-twitter"></i>
-            </a>
-            <a href="">
-              <i className="fa fa-linkedin"></i>
-            </a>
+          <div className="col-xs-12 col-md-7">
+            <div className="row">
+              <div className="block-footer col-xs-7 about-description"
+                   style={{color: '#2C3E50', paddingLeft: '0px'}}>
+                <span>E-mail: orsolya.birkas@adecco.com<br></br>
+                Telefon: 06308689742</span>
+              </div>
+              <div className="block-footer col-xs-1 col-xs-offset-1"
+                   style={{background: '#3b5998'}}>
+                <a href="">
+                  <i className="fa fa-facebook social-icon"></i>
+                </a>
+              </div>
+              <div className="block-footer col-xs-1"
+              style={{background: '#4099FF'}}>
+                <a href="">
+                  <i className="fa fa-twitter social-icon"></i>
+                </a>
+              </div>
+              <div className="block-footer col-xs-1"
+              style={{background: '#0e76A8'}}>
+                <a href="">
+                  <i className="fa fa-linkedin social-icon"></i>
+                </a>
+              </div>
+              <div>
+              <h6 style={{color: '#2C3E50'}}>Webdesign & sitebuild: E_Bro</h6>
+              </div>
+            </div>
           </div>
         </div>
       </div>
