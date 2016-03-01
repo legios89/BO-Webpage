96b54803774beff400233558b2d9b459f5af91ca
Egyed Zoltán <zoltan.egyed@vertis.com>
2016-03-01 21:04:11 +0100

    update

--- react/components/parts/job.jsx
+++ react/components/parts/job.jsx
@@ -9,9 +9,36 @@ var Job = React.createClass({
 
   render: function () {
     return (
-      <div className="block-job col-lg-3 col-md-4 col-sm-6 col-xs-12"
+      <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12"
            style={{background: this.props.color}}>
-           {this.props.job.title}
+        <div className="portlet light portlet-fit bordered">
+          <div className="portlet-title">
+            <div className="caption">
+              <i className=" icon-layers font-green"></i>
+              <span className="caption-subject font-green bold uppercase">
+                <div className="caption-desc font-grey-cascade">
+                  <pre className="mt-code">{this.props.job.title}</pre></div>
+              </span>
+            </div>
+          </div>
+          <div className="portlet-body">
+            <div className="mt-element-overlay">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="mt-overlay-4">
+                    <img src="/static/img/06.jpg" />
+                    <div className="mt-overlay">
+                      <h2>{this.props.job.title}</h2>
+                      <a className="mt-info btn btn-default" href="#">
+                        Learn More
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
